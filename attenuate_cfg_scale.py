6a4aaa00f2dc19170caa994305380fef27edd69e
tkalayci71 <114084637+tkalayci71@users.noreply.github.com>
2022-11-29 01:35:14 +0300

    fix for more samplers. extend strength range to 2

--- attenuate_cfg_scale.py
+++ attenuate_cfg_scale.py
@@ -1,7 +1,7 @@
 # Attenuate CFG Scale script for AUTOMATIC1111/stable-diffusion-webui
 #
 # https://github.com/tkalayci71/attenuate-cfg-scale
-# version 1.4 - 2022.11.21
+# version 1.5 - 2022.11.29
 #
 # also check out:
 # https://github.com/guzuligo/CFG-Schedule-for-Automatic1111-SD
@@ -25,7 +25,7 @@ class Script(scripts.Script):
         return not(is_img2img)
 
     def ui(self, is_img2img):
-        strength = gr.Slider(value=1.00, label="Strength", minimum=0.00, maximum=1.00,   step=0.01)
+        strength = gr.Slider(value=1.00, label="Strength", minimum=0.00, maximum=2.00, step=0.1)
         return [strength]
 
     def run(self, p, strength):
@@ -33,17 +33,18 @@ class Script(scripts.Script):
         if p.sampler_name in ('Euler a','Euler','LMS','DPM++ 2M','DPM fast','LMS Karras','DPM++ 2M Karras'):
             max_mul_count = p.steps * p.batch_size
             steps_per_mul = p.batch_size
-        elif p.sampler_name in ('Heun','DPM2','DPM2 a','DPM++ 2S a','DPM2 Karras','DPM2 a Karras','DPM++ 2S a Karras'):
+        elif p.sampler_name in ('Heun','DPM2','DPM2 a','DPM++ 2S a','DPM2 Karras','DPM2 a Karras','DPM++ 2S a Karras','DPM++ SDE','DPM++ SDE Karras'):
             max_mul_count = ((p.steps*2)-1) * p.batch_size
             steps_per_mul = 2 * p.batch_size
-        elif p.sampler_name=='DDIM': # ddim
+        elif p.sampler_name=='DDIM':
             max_mul_count = fix_ddim_step_count(p.steps)
             steps_per_mul = 1
-        elif p.sampler_name=='PLMS': # plms
+        elif p.sampler_name=='PLMS':
             max_mul_count = fix_ddim_step_count(p.steps)+1
             steps_per_mul = 1
         else:
-            return # 9=dpm adaptive
+            print('!!!warning: unsupported sampler ',p.sampler_name)
+            return
 
         target_value = p.cfg_scale * (1-strength)
         saved_obj = p.cfg_scale
